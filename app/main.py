77a5a417be585a909c553244afa8270bcff5d593
Alex <alex@bp-architecture.local>
2025-11-30 21:26:58 +0100

    Implementation of os and subprocess modules

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 import sys
+import os
+import subprocess
 
 commands = ['exit','echo','type']
 def main(commands):
+    
     while True:
         sys.stdout.write("$ ")
         command = input()
@@ -19,11 +22,22 @@ def main(commands):
                         print(f'{cmd} is a shell builtin')
             if str1 not in commands:
                  print(f'{str1}: not found')
-                
+         
         else:
+            path = os.environ.get('PATH','')
+            directories = path.split(':')
+            cmd_parts = command.split()
+            cmd_name = cmd_parts[0]
+            found = False
+            for directory in directories:
+                 full_path = os.path.join(directory,cmd_name)
+                 if os.path.isfile(full_path) and os.access(full_path,os.X_OK):
+                      subprocess.run([full_path]+ cmd_parts[1:])
+                      found = True
+                      break
             print(f"{command}: command not found")
         pass
-
+        
 
 if __name__ == "__main__":
         main(commands)
